9f6120eb968bd976e417b387d8a0758be99b55d6
potatozhb <zhaohongbo1980@gmail.com>
2025-03-16 22:12:40 -0700

    2

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	godotenv.Load()
+
 	fmt.Println("Welcome to our conference booking application")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	fmt.Println("Listening on port: ", port)
+
 }
